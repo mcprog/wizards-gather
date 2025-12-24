78c50643cf9411b70ae6d94740cb22d93628090a
shirograhm <xavierwg13@gmail.com>
2025-12-23 18:30:27 -0800

    Fix for me getting the files backward

--- enemies/base_enemy.gd
+++ enemies/base_enemy.gd
@@ -1,20 +1,18 @@
 class_name BaseEnemy extends Node2D
 
 
-@export_subgroup("Nodes")
-@export var animation_component: AnimationComponent
-
-@onready var statistics: UnitStatistics = $UnitStatistics
-@onready var sprite: AnimatedSprite2D = $Sprite
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	animation_component.play_move_animation()
+	pass
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
 
+
 func _physics_process(_delta: float) -> void:
+	pass
+
+
+## Example as to why we have this class
+func death() -> void:
 	pass
--- units/example_unit.gd
+++ units/example_unit.gd
@@ -1,13 +1,21 @@
 class_name ExampleUnit extends BaseUnit
 
 
+@export_subgroup("Nodes")
+@onready var animation_component: AnimationComponent = $AnimationComponent
+
+@onready var statistics: UnitStatistics = $UnitStatistics
+@onready var sprite: AnimatedSprite2D = $Sprite
+
+# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	self.statistics.prepare(150.0, 15.0, 0.01, 1.75, 20.0, 1.33, 33)
+	animation_component.play_move_animation()
+	statistics.prepare(150.0, 15.0, 0.01, 1.75, 20.0, 1.33, 33)
 
 
+# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
-
 
 func _physics_process(_delta: float) -> void:
 	pass

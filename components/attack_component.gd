a094e2343a3460ec53a0ccede1bbc8bcd9570559
shirograhm <xavierwg13@gmail.com>
2026-01-19 15:14:31 -0800

    Formatting and comments

--- components/attack_component.gd
+++ components/attack_component.gd
@@ -23,6 +23,8 @@ var target: Node2D = null
 
 var rng = RandomNumberGenerator.new()
 
+
+## Prepare the component statistics for the unit. This must be called on the unit's _ready()
 func prepare(dam: float, crit_c: float, crit_d: float, mana: float, attacks_per_second: float, mana_per_second: float, range_in: float) -> void:
 	self.damage = dam
 	self.crit_chance = crit_c
@@ -43,12 +45,14 @@ func _process(delta: float) -> void:
 	process_targeting()
 	process_mana_regen(delta)
 
+
 ## Attempts to select target for the unit attached
 func process_targeting() -> void:
 	if target == null:
 		target = select_target()
 	# Set has_target bool
 	has_target = (target != null)
+
 
 ## Process mana regen per frame. Handles has_max_mana variable
 func process_mana_regen(delta: float) -> void:
@@ -62,12 +66,12 @@ func process_mana_regen(delta: float) -> void:
 			current_mana += mana_per_frame
 
 
-
-
+## Rolls crit based on component's crit chance
 func roll_crit() -> bool:
 	return rng.randf() < crit_chance
-	
 
+	
+## Returns attack speed as a duration to be used in timer.start(_) 
 func get_attack_speed_as_duration() -> float:
 	return 1.0 / attack_speed
 
